817e497e12caeef3dfa8e4000175775d2ec1d473
Reddiepoint <31847741+Reddiepoint@users.noreply.github.com>
2024-02-06 15:08:44 +0800

    Add function to write changes to file so they can be downloaded

--- src/modules/depot_downloader.rs
+++ src/modules/depot_downloader.rs
@@ -1,3 +1,4 @@
+use std::process::Command;
 use crate::modules::changes::Changes;
 
 pub struct DepotDownloaderSettings {
@@ -16,6 +17,18 @@ impl Default for DepotDownloaderSettings {
     }
 }
 
-pub fn download_changes(changes: &Changes, settings: &DepotDownloaderSettings) {
-    todo!()
+fn write_changes_to_file(changes: &Changes) -> std::io::Result<()> {
+    let download_files = changes.added.join("\n") + &changes.modified.join("\n");
+    // Write changes to file files.txt
+    let path = "files.txt";
+    std::fs::write(path, download_files)?;
+    Ok(())
+}
+pub fn download_changes(changes: &Changes, settings: &DepotDownloaderSettings) -> std::io::Result<()> {
+    write_changes_to_file(changes)?;
+    // Run Depot Downloader
+    // Command::new("./DepotDownloader.exe")
+    //     .args()
+
+    Ok(())
 }
